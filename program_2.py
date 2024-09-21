e90dde18402cb2ba012684432a7e7d11ed0fded9
ClaraDLoon <ClaraDLoon@outlook.com>
2024-09-20 22:45:05 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,27 +1,23 @@
-# Write a program that asks the user to enter a person's age.  The program should display a message indicating whether the person is an infant, a child, a teenager, or an adult.  Following are the guidelines:
+#COS2005 Python Programming
+#Luke Snell
+#Clara Riley
+#09/20/24
+#Program 2
 
-# If the person is 1 year old or less, it should display "infant" (without quotes).
-# If the person is older than 1 year, but younger than 13 years, it should display "child".
-# If the person is at least 13 years old, but less than 20 years old, it should display "teenager".
-# If the person is at least 20 year old, it should display "adult".
-
-def categorize_age(age):
-    ageCategory = "TBD"
-    ######################
-    # WRITE YOUR CODE HERE
-    ######################
-
-
-    return ageCategory
-
-
-#### This piece of the code has been done for you,
-#### you only need to worry about the actual shipping 
-#### charge logic in the weight_conversion function
-if __name__ == '__main__':
-    # Local variables
-    # Get age from the user.
-    age = float(input("Enter the person's age: "))
-    # Display the age
-    ageBucket = categorize_age(age)
-    print (ageBucket)
+#Prompt for age
+age= int(input('Enter age:'))
+#If person is greater than or equal to 1, display "infant."
+if age <= 1:
+    print('infant')
+#If person is greater than 1 but less than 13, display "child."
+elif age > 1 and age < 13:
+    print('child')
+#If person is greater than 12 but less than 20, display "teenager."
+elif age > 13 and age < 20:
+    print('teenager')
+#If person is greater than or equal to 20, display "adult."
+elif age >= 20 and age < 125:
+    print('adult')
+#If other, display "You are Dead."
+else:
+    print('You are dead.')
